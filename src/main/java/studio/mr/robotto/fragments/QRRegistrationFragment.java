012e73678f6513f2ac3b0f5cba2bff625a25a2a7
Aarón Negrín <negrinaaron@gmail.com>
2015-11-24 10:42:39 +0100

    Addition of qrregistration, sometimes git makes stupid things

--- src/main/java/studio/mr/robotto/fragments/QRRegistrationFragment.java
+++ src/main/java/studio/mr/robotto/fragments/QRRegistrationFragment.java
@@ -1,88 +1,81 @@
 package studio.mr.robotto.fragments;
 
-import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import eu.livotov.zxscan.ScannerView;
 import studio.mr.robotto.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link QRRegistrationFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link QRRegistrationFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class QRRegistrationFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class QrRegistrationFragment extends Fragment implements View.OnClickListener, ScannerView.ScannerViewEventListener {
 
     private OnFragmentInteractionListener mListener;
+    private ScannerView mScannerView;
+    private boolean mScannerStarted = false;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment QRRegistrationFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static QRRegistrationFragment newInstance(String param1, String param2) {
-        QRRegistrationFragment fragment = new QRRegistrationFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+
+    public QrRegistrationFragment() {
+        // Required empty public constructor
     }
 
-    public QRRegistrationFragment() {
-        // Required empty public constructor
+    public static QrRegistrationFragment newInstance() {
+        QrRegistrationFragment fragment = new QrRegistrationFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_qrregistration, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_qrregistration, container, false);
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+        mScannerView = (ScannerView) view.findViewById(R.id.scanner);
+        mScannerView.setScannerViewEventListener(this);
+        //mScannerView.startScanner();
+        mScannerStarted = false;
+
+        Button qrStartBtn = (Button) view.findViewById(R.id.start_qr_register_button);
+        qrStartBtn.setOnClickListener(this);
+        Button qrStopBtn = (Button) view.findViewById(R.id.stop_qr_register_button);
+        qrStopBtn.setOnClickListener(this);
+        return view;
     }
 
     @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        try {
-            mListener = (OnFragmentInteractionListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnFragmentInteractionListener");
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        if (this.isVisible()) {
+            // If we are becoming invisible, then...
+            if (!isVisibleToUser) {
+                stopScanner();
+            }
+        }
+    }
+
+    private void stopScanner() {
+        if (mScannerStarted) {
+            mScannerView.stopScanner();
+            mScannerStarted = false;
+        }
+    }
+
+    private void startScanner() {
+        if (!mScannerStarted) {
+            mScannerView.startScanner();
+            mScannerStarted = true;
         }
     }
 
@@ -92,19 +85,35 @@ public class QRRegistrationFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.start_qr_register_button) {
+            startScanner();
+        } else if (view.getId() == R.id.stop_qr_register_button) {
+            stopScanner();
+        }
+    }
+
+    @Override
+    public void onScannerReady() {
+
+    }
+
+    @Override
+    public void onScannerFailure(int i) {
+
+    }
+
+    @Override
+    public boolean onCodeScanned(String data) {
+        mScannerView.stopScanner();
+        Toast.makeText(getActivity(), "Data scanned: " + data, Toast.LENGTH_SHORT).show();
+        return true;
+    }
+
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
+        void onFragmentInteraction(Uri uri);
     }
 
 }
